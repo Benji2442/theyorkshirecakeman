179aa656de17b8b0172fd18ad832bf21cb3349ee
Ben Hall <Benjamin2442@hotmail.co.uk>
2017-04-21 19:59:06 +0100

    Added splash screen - WORKING, but also added prepended div for the splash image - NOT WORKING YET

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@ $(".image-section").hide().slideDown("slow");
 
 $("#navbar").children().hide().fadeIn(2500);
 
-$("body").prepend('<div id="splashscreen"></div>');
+//adding splash screen
+$("body").append('<div id="splashscreen"></div>');
+
+//adding image to splash
+$("#splashscreen").prepend('<div id="splash"></div>')
+
+//stops scroll bar working
+$('html, body').css({
+    overflow: 'hidden',
+    height: '100%'
+});
