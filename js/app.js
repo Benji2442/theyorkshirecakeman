86dec30ff481e18f9a5e101f5076ec20bccb8e07
Ben Hall <Benjamin2442@hotmail.co.uk>
2017-04-25 22:02:31 +0100

    fixed issue with mobile nav showing on large screens

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 // jquery files
 
 
-$(".image-section").hide().slideDown("slow");
-
-$("#navbar").children().hide().fadeIn(2500);
-
 //adding splash screen
 $("body").append('<div id="splashscreen"><img id="splash-logo" src="img/logofinal.svg"></div>');
 
@@ -14,15 +10,14 @@ $('body').addClass('stop-scrolling')
 //hiding nav-mobile button
 $(".nav-mobile").hide();
 
-
+//fades #logo on splashcreen
 $("#splashscreen img").hide().fadeIn(3000);
 
 //$("#splashscreen").append("<h1>Welcome to the Yorkshire cake man</h1>")
 $("#splashscreen").append("<button type='button' class='btn center-block btn-lg btn-default enter-site'>Enter site</button>");
 
-
-
-$(".enter-site").click(function(){
+//enter site button to close splashscreen and start website animations
+$(".enter-site").one("click", function(){
 	$("#splashscreen").hide();
 	$(".nav-mobile").show();
 	$('body').removeClass('stop-scrolling')
